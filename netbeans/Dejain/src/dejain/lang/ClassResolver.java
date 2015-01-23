3321a417a0286b65a9091c44eec3d6ad50282f70
jakobehmsen <jakobehmsen.github@gmail.com>
2015-01-23 16:20:27 +0100

    Generalized ClassResolver.

--- netbeans/Dejain/src/dejain/lang/ClassResolver.java
+++ netbeans/Dejain/src/dejain/lang/ClassResolver.java
@@ -1,31 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package dejain.lang;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class ClassResolver {
-    private ClassMap classMap;
-    private Map<String, String> imports = new HashMap<>();
-
-    public ClassResolver(ClassMap classMap) {
-        this.classMap = classMap;
-    }
-    
-    public void importPackage(String packageName) {
-        for(String className: classMap.getClassNames(packageName))
-            imports.put(className, packageName + "." + className);
-    }
-    
-    public String resolveClassName(String className) {
-        String importedName = imports.get(className);
-        
-        if(importedName != null)
-            return importedName;
-        
-        return className;
-    }
-    
-    public Class<?> resolveType(String typeName) throws ClassNotFoundException {
+/**
+ *
+ * @author Jakob
+ */
+public interface ClassResolver {
+    default Class<?> resolveType(String typeName) throws ClassNotFoundException {
         switch(typeName) {
             case "boolean": return boolean.class;
             case "byte": return byte.class;
@@ -38,14 +23,12 @@ public class ClassResolver {
         
         String className = resolveClassName(typeName);
         
-        if(className == null)
-            System.out.println("className=" + className + ",imports=" + imports);
-        
         try {
             return Class.forName(className);
         } catch(ClassNotFoundException e) {
-            System.out.println("error: className=" + className + ",imports=" + imports);
             throw e;
         }
     }
+
+    String resolveClassName(String className);
 }
